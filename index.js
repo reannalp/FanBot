125dc32e42c7481ede4246c370bb9d104c54dc40
Reanna <reannalacasse@gmail.com>
2020-03-22 18:18:58 -0700

    dependencies

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const Discord = require('discord.js');
+const config = require('./config.json');
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', (message) => {
+  if (message.author.bot) return;
+  const args = message.content.split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if (command === 'ping') {
+    message.channel.send('...').then((msg) => {
+      const latency = msg.createdTimestamp / 1000 - message.createdTimestamp / 1000;
+      msg.edit(`Latency: ${latency.toFixed(3)} seconds\nAPI Latency: ${Math.round(client.ws.ping) / 1000} seconds`);
+    });
+  }
+  if (command === 'say') {
+    message.channel.send(args.join(' '));
+    message.delete();
   }
 });
 
-client.login('NTY0OTkxMjg2ODUyMDU5MTM2.XnfvQg.5bUeGYpgMNFXe07xTIv5GXmFT18');
+client.login(config.token);
