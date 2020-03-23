5f5cf253a460d4805441312feaf71aa3aa08da8b
Reanna <reannalacasse@gmail.com>
2020-03-23 00:26:49 -0700

    syntax

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'say',
   description: 'Say!',
-  execute(client, message, args) {
+  execute(message, args) {
     // if (message.author.id !== client.config.ownerID) return;
     message.channel.send(args.join(' '));
     message.delete();
