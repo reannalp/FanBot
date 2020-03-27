57a4576c5ef82279de9fd8e1e7e3440fa48a3035
Reanna <reannalacasse@gmail.com>
2020-03-27 16:02:02 -0700

    react w/ args

--- commands/react.js
+++ commands/react.js
@@ -1,7 +1,9 @@
 module.exports = {
   name: 'react',
   description: 'testing command, adds a reaction to message',
+  args: true,
+  usage: '<emojiname>',
   execute(client, message, args) {
-    message.react(client.emojis.cache.find((x) => x.name === 'sanime'));
+    message.react(client.emojis.cache.find((x) => x.name === args.toString()));
   },
 };
