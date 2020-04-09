4f6f2620b6d2b4115a3b9278a9660c6808b8732d
Reanna <reannalacasse@gmail.com>
2020-04-08 18:33:09 -0700

    role error handled

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,7 +1,7 @@
 module.exports = (client, reaction, user) => {
   if (!user.bot && reaction.message.embeds[0].title === 'Roles' && reaction.emoji.id === '403412254562713600') {
     const addRole = reaction.message.guild.roles.cache.find((cb) => cb.name === 'Artist');
-    // add in an error handler
+    if (!addRole) return reaction.message.channel.send('That role does not exist on this server.');
     reaction.message.guild.members.cache.get(user.id).roles.add(addRole)
       .then(reaction.message.channel.send(`Added ${addRole.toString()} role to ${user.username}`))
       .catch(console.error);
