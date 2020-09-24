2ea83022fc5ed416328b2bb8ea5a90786d69bb57
reannalp <thebestreanna@gmail.com>
2020-09-24 16:39:38 -0700

    remove remote files

--- commands/wog1.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'wog1',
-  description: '',
-  execute(client, message, args) {
-    message.channel.send({ files: ['./assets/ProfoundBond.png'] });
-    message.delete();
-  },
-};
